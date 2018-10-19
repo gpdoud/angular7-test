b26a5b37bb534949e25bbe87fc302ceb3e711016
Greg Doud <gpdoud@gmail.com>
2018-10-19 14:59:45 -0400

    initial commit

---
+++ src/app/user/user-detail/user-detail.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { UserService } from '../user.service';
+import { User } from '../user.class';
+import { JsonResp } from '../../jsonresp.class';
+
+@Component({
+  selector: 'app-user-detail',
+  templateUrl: './user-detail.component.html',
+  styleUrls: ['./user-detail.component.css']
+})
+export class UserDetailComponent implements OnInit {
+
+  user: User;
+
+  constructor(
+    private usersvc: UserService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit() {
+    const id = this.route.snapshot.params.id;
+    this.usersvc.get(id)
+      .subscribe(resp => {
+        console.log('JsonResp:', resp);
+        this.user = resp.data;
+      });
+  }
+
+}
